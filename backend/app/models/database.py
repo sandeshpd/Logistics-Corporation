5c5f2e2c9d0bf2837dbeedf98b884e90b87d1cb6
sandeshpd <sandesh@sandesh.com>
2025-07-14 18:05:39 +0530

    database and pydantic models

---
+++ backend/app/models/database.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy.ext.declarative import declarative_base
+from typing import List
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    full_name = Column(String)
+    role = Column(String)   # E.g. driver, manager etc.
+    jobs = relationship("Job", back_populates="driver")
+
+class Vehicle(Base):
+    __tablename__ = "vehicles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    license_plate = Column(String, unique=True, index=True)
+    model = Column(String)
+    vehicle_type = Column(String)   # e.g. freight train, trailer
+    capacity = Column(Float)    # e.g. in tons
+    jobs = relationship("Job", back_populates="vehicle")
+
+class Job(Base):
+    __tablename__ = "jobs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    description = Column(String)
+    source_location = Column(String)
+    destination_location = Column(String)
+    scheduled_time = Column(DateTime)
+    driver_id = Column(Integer, ForeignKey("users.id"))
+    # driver = relationship("User", back_populates="jobs")
+    driver: Mapped["User"] = relationship(back_populates="jobs")
+    vehicle_id = Column(Integer, ForeignKey("vehicles.id"), nullable=True)
+    driver_name = Column(String, nullable=True)    # Denormalized for convenience
+    vehicle = relationship("Vehicle", back_populates="jobs")
---
+++ backend/app/models/schemas.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel
+from typing import List
+from datetime import datetime
+
+# Base Models 
+class UserBase(BaseModel):
+    username: str
+    full_name: str
+    role: str
+
+class VehicleBase(BaseModel):
+    license_plate: str
+    model: str
+    vehicle_type: str
+    capacity: float
+
+class JobBase(BaseModel):
+    description: str
+    source_location: str
+    destination_location: str
+    scheduled_time: datetime
+    driver_id: int
+
+
+# Response Models
+class UserResponse(UserBase):
+    jobs: List[JobBase]
+
+class VehicleResponse(BaseModel):
+    model: str
+    vehicle_type: str
+    capacity: float
+    jobs: List[JobBase]
+
+class JobResponse(JobBase):
+    id: int
+    source_location: str
+    destination_location: str
+    scheduled_time: datetime
+    # Currently this is One-to-One relationship because I initially thought One job 
+    # has only One driver, but drivers' shifts change in long journey.
+    # FIXME: Change it in Many-to-Many relationship.
+    driver: UserBase    
