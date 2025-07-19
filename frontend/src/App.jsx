e49e622371361ccb98e098fef4a51848d78d002a
sandeshpd <sandesh@sandesh.com>
2025-07-19 23:10:03 +0530

    Added new routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import './App.css'
+import Login from './pages/Login';
+import Home from "./pages/Home";
+import Jobs from "./pages/Jobs";
+import NotFound from "./pages/NotFound";
 
 function App() {
 
   return (
     <>
-    <h1>Logistics Corporation is in construction. It'll be operational soon...</h1>      
+      <ChakraProvider value={defaultSystem}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ChakraProvider>
     </>
   )
 }
 
-export default App
+export default App;
