2634265e5e1d023b3217b588d3ee472691c4ce57
sandeshpd <sandesh@sandesh.com>
2025-07-20 15:52:43 +0530

    Added external Sign-up links

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,14 +6,18 @@ import {
     Container,
     Field,
     HoverCard,
+    HStack,
     Icon,
     Input,
     Link,
     Portal,
+    Separator,
     Text,
     VStack
 } from "@chakra-ui/react";
 import { FiAlertCircle } from "react-icons/fi";
+import { FaGoogle } from "react-icons/fa";
+import { FaXTwitter, FaFacebookF } from "react-icons/fa6";
 import profile_picture_src from "../assets/default_profile_picture.png";
 import "../css/Login.css";
 
@@ -39,7 +43,11 @@ function Login() {
                                     <Field.Label>
                                         Username <Field.RequiredIndicator />
                                     </Field.Label>
-                                    <Input placeholder="Enter your username" variant="outline" />
+                                    <Input
+                                        placeholder="Enter your username"
+                                        variant="outline"
+                                        paddingLeft=".9em"
+                                    />
                                 </Field.Root>
                                 <Field.Root>
                                     <Field.Label>
@@ -49,43 +57,101 @@ function Login() {
                                         type="password"
                                         placeholder="Enter your password"
                                         variant="outline"
+                                        paddingLeft=".9em"
                                     />
                                 </Field.Root>
                             </VStack>
-                            <Text justifyContent="end" fontSize="1rem">
-                                Don't have an account?{" "}
-                                <Link
-                                    variant="underline"
-                                    href="#"
-                                    colorPalette="blue"
-                                >
-                                    Create one.
-                                    <HoverCard.Root
-                                        positioning={{ placement: "top" }}
-                                        size="sm"
-                                        open={open}
-                                        onOpenChange={(e) => setOpen(e.open)}>
-                                        <HoverCard.Trigger asChild>
-                                            <Icon size="xs" color="#888">
-                                                <FiAlertCircle />
-                                            </Icon>
-                                        </HoverCard.Trigger>
-                                        <Portal>
-                                            <HoverCard.Positioner>
-                                                <HoverCard.Content>
-                                                    <HoverCard.Arrow />
-                                                    <Text textStyle="sm" color="fg.muted" p="5px">
-                                                        Only Admin users can log in.
-                                                    </Text>
-                                                </HoverCard.Content>
-                                            </HoverCard.Positioner>
-                                        </Portal>
-                                    </HoverCard.Root>
-                                </Link>
-                            </Text>
                         </Card.Body>
                         <Card.Footer justifyContent="center">
-                            <Button backgroundColor="#1563bd">Sign In</Button>
+                            <VStack>
+                                <Button
+                                    backgroundColor="#1563bd"
+                                    mt="1em"
+                                >
+                                    Sign In
+                                </Button>
+                                <Text justifyContent="end" fontSize="1rem">
+                                    Don't have an account?{" "}
+                                    <Link
+                                        variant="underline"
+                                        href="#"
+                                        colorPalette="blue"
+                                    >
+                                        Create one.
+                                        <HoverCard.Root
+                                            positioning={{ placement: "right" }}
+                                            size="sm"
+                                            open={open}
+                                            onOpenChange={(e) => setOpen(e.open)}>
+                                            <HoverCard.Trigger asChild>
+                                                <Icon size="xs" color="#888">
+                                                    <FiAlertCircle />
+                                                </Icon>
+                                            </HoverCard.Trigger>
+                                            <Portal>
+                                                <HoverCard.Positioner>
+                                                    <HoverCard.Content>
+                                                        <HoverCard.Arrow />
+                                                        <Text textStyle="sm" color="fg.muted" px="9px">
+                                                            Only Admin users can log in.
+                                                        </Text>
+                                                    </HoverCard.Content>
+                                                </HoverCard.Positioner>
+                                            </Portal>
+                                        </HoverCard.Root>
+                                    </Link>
+                                </Text>
+                                <HStack>
+                                    <Separator 
+                                        flex="1" 
+                                        orientation="horizontal"    
+                                        variant="solid" 
+                                        color="black" 
+                                        size="lg"
+                                    />
+                                    <Text flexShrink="0">Or</Text>
+                                    <Separator 
+                                        flex="1" 
+                                        orientation="horizontal"
+                                        variant="solid" 
+                                        color="black" 
+                                        size="lg"
+                                    />
+                                </HStack>
+                                <Text>Login using:</Text>
+                                <HStack gap="6">
+                                    <Button
+                                        color="#000"
+                                        background="#fff"
+                                        border="1px solid"
+                                        borderRadius="50%"
+                                        w="3rem"
+                                        h="3rem"
+                                    >
+                                        <FaGoogle />
+                                    </Button>
+                                    <Button
+                                        color="#000"
+                                        background="#fff"
+                                        border="1px solid"
+                                        borderRadius="50%"
+                                        w="3rem"
+                                        h="3rem"
+                                    >
+                                        <FaFacebookF />
+                                    </Button>
+                                    <Button
+                                        color="#000"
+                                        background="#fff"
+                                        border="1px solid"
+                                        borderRadius="50%"
+                                        w="3rem"
+                                        h="3rem"
+                                    >
+                                        <FaXTwitter />
+                                    </Button>
+                                </HStack>
+                            </VStack>
                         </Card.Footer>
                     </Card.Root>
                 </form>
