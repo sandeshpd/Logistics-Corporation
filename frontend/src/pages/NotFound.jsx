adf530c8f76b852998c1596bf4461ddf273717fe
sandeshpd <sandesh@sandesh.com>
2025-07-20 15:53:24 +0530

    Centered main content

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,13 +1,16 @@
+import { Center } from "@chakra-ui/react";
 import not_found from "../assets/404-not-found.jpg";
 import "../css/NotFound.css";
 
 const NotFound = () => {
     return (
         <>
-        <div className="main_container">
-            <p>Error 404 - Page you are looking for is not found. It might have been moved or deleted.</p>
-            <img src={not_found} height="400px" width="400px"/>
-        </div>
+            <Center>
+                <div className="main_container">
+                    <p>Error 404 - Page you are looking for is not found. It might have been moved or deleted.</p>
+                    <img src={not_found} height="400px" width="400px" />
+                </div>
+            </Center>
         </>
     )
 }
