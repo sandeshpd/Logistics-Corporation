68afe683e7c596629072ad5117b6d13d717c1bac
sandeshpd <sandesh@sandesh.com>
2025-07-26 14:15:16 +0530

    Added Login & Register buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import './App.css'
 import Login from './pages/Login';
@@ -6,14 +6,20 @@ import Home from "./pages/Home";
 import Jobs from "./pages/Jobs";
 import NotFound from "./pages/NotFound";
 import Register from "./pages/Register";
+import UnderConstruction from "./pages/UnderConstruction";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const location = useLocation();
+  const hideNavbar = location.pathname==="/login" || location.pathname==="/register";
 
   return (
     <>
       <ChakraProvider value={defaultSystem}>
+        {!hideNavbar && <Navbar />}
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<UnderConstruction />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/jobs" element={<Jobs />} />
           <Route path="/register" element={<Register />} />
