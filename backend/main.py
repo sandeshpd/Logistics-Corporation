317154927ea4b04a0861f53c448edb08357510cb
sandeshpd <sandesh@sandesh.com>
2025-08-22 21:05:41 +0530

    removed origins list

--- backend/main.py
+++ backend/main.py
@@ -8,18 +8,20 @@ app = FastAPI(
     version="1.0.0"
 )
 
-origins = [
-    "http://localhost:5173",
-    "localhost:5173"
-]
+# origins = [
+#     "http://localhost:5173",
+#     "http://localhost:5173/",
+#     "http://127.0.0.1:5173",
+#     "localhost:5173/",
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+#     allow_headers=["*"],
+# )
 
 @app.get("/", tags=["root"])
 async def index():
