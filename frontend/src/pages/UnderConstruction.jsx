d3e6280cb1e35d911cca6eba4a0341c5ed52f08e
sandeshpd <sandesh@sandesh.com>
2025-08-22 21:10:58 +0530

    added nice UI

--- frontend/src/pages/UnderConstruction.jsx
+++ frontend/src/pages/UnderConstruction.jsx
@@ -1,14 +1,48 @@
-import { Container, Image } from "@chakra-ui/react";
+import { Container, HStack, Image, Link, Stack, VStack } from "@chakra-ui/react";
 import under_construction from "../assets/under-construction.png";
+import "../css/UnderConstruction.css";
 
 
 function UnderConstruction() {
     return (
         <>
-            <Container display="grid" justifyItems="center">
+            <Container display="grid" justifyItems="center" h="100%">
                 <h1>This website is under construction. Stay tuned.</h1>
-                <Image src={under_construction} height="400px" width="auto" mt="1.5em" />
+                <Stack direction="row" gap="19rem">
+                    <Image src={under_construction} height="300px" width="auto" mt="1.5em" />
+                    <VStack
+                        width="20rem"
+                        height="fit-content"
+                        mt="1.5em"
+                        py=".5em"
+                        border="1px solid #000"
+                        borderRadius="12px"
+                    >
+                        <h2>Meanwhile checkout these links:</h2>
+                        <ul>
+                            <li>
+                                <Link href="/home">Home</Link>
+                            </li>
+                            <li>
+                                <Link href="/jobs">Jobs</Link>
+                            </li>
+                            <li>
+                                <Link href="/users">Users</Link>
+                            </li>
+                            <li>
+                                <Link href="/vehicles">Vehicles</Link>
+                            </li>
+                            <li>
+                                <Link href="/login">Login</Link>
+                            </li>
+                            <li>
+                                <Link href="/register">Register</Link>
+                            </li>
+                        </ul>
+                    </VStack>
+                </Stack>
             </Container>
+
         </>
     )
 }
